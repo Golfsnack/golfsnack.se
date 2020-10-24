c988113df0dd7aeccdb32b2117d0662483502d08
Kim Fransman <kim.fransman@gmail.com>
2020-10-24 14:16:23 +0200

    Disabled STRAPI guides for now

--- app/models/guide.rb
+++ app/models/guide.rb
@@ -1,16 +1,18 @@
-require 'faraday'
-API_URL = "http://cms.golfsnack.se"
+# require 'faraday'
+# API_URL = "http://cms.golfsnack.se"
 
 class Guide
 
   def self.latest
-    resp = Faraday.get "#{API_URL}/guides"
-    JSON.parse(resp.body)
+    # resp = Faraday.get "#{API_URL}/guides"
+    # JSON.parse(resp.body)
+    []
   end
 
   def self.by_id(id)
-    resp = Faraday.get "#{API_URL}/guides/#{id}"
-    JSON.parse(resp.body)
+    # resp = Faraday.get "#{API_URL}/guides/#{id}"
+    # JSON.parse(resp.body)
+    nil
   end
 
 end
