525f666a5f94c4c5002e487c20b47bc2cc893585
Kim Fransman <kim.fransman@gmail.com>
2020-10-24 15:00:04 +0200

    Added SSO for discourse

---
+++ app/controllers/discourse_sso_controller.rb
@@ -0,0 +1,20 @@
+require 'single_sign_on'
+
+class DiscourseSsoController < ApplicationController
+  before_action :authenticate_user! # ensures user must login
+
+  def sso
+    secret = ENV['SSO_SECRET']
+    sso = SingleSignOn.parse(request.query_string, secret)
+    sso.email = current_user.email # from devise
+    sso.name = current_user.name # this is a custom method on the User class
+    sso.username = current_user.email # from devise
+    sso.external_id = current_user.id # from devise
+    if current_user.avatar?
+      sso.avatar_url = current_user.avatar_url(:thumb)
+    end
+    sso.sso_secret = secret
+
+    redirect_to sso.to_url("http://forum.golfsnack.se/session/sso_login")
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 
+  get 'discourse/sso' => 'discourse_sso#sso'
+
   get '/admin', to: 'admin#index'
   post '/admin', to: 'admin#create', as: :create_invitation
 
